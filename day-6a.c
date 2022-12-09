4320f74d63ba007ca5445f4a89df624c3532c212
nkraetzschmar <9020053+nkraetzschmar@users.noreply.github.com>
2022-12-10 00:04:21 +0100

    day-6

---
+++ day-6a.c
@@ -0,0 +1,49 @@
+int32_t check_marker(uint8_t *word)
+{
+	for (uint64_t i = 0; i < 4; ++i)
+	{
+		if(!word[i]) return 0;
+		for (uint64_t j = i+1; j < 4; ++j)
+		{
+			if (word[i] == word[j]) return 0;
+		}
+	}
+	return 1;
+}
+
+void push_word(uint8_t *word, uint8_t c)
+{
+	for (uint64_t i = 0; i < 3; ++i) word[i] = word[i+1];
+	word[3] = c;
+}
+
+void init()
+{
+	uint8_t buf[0x100];
+	uint8_t word[4];
+
+	uint8_t *ptr;
+	uint64_t len = 0;
+	uint64_t pos = 0;
+
+	while (!check_marker(word))
+	{
+		if (len == 0)
+		{
+			len = read(0, buf, sizeof(buf));
+			if (!len) exit(1);
+			ptr = buf;
+		}
+
+		push_word(word, *(ptr++));
+		--len;
+		++pos;
+	}
+
+	uint64_t write_len = format_dec(pos, buf, sizeof(buf));
+	if (!write_len) exit(1);
+	buf[write_len-1] = '\n';
+	write(1, buf, write_len);
+
+	exit(0);
+}
